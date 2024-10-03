dbffc4c2e9ae6c54c660fc48f239456b6d3902a0
Owais-Sonija <owaissonija007@gmail.com>
2024-10-03 22:35:17 +0500

    Added copying function

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+// Getting Elements
+const passwordEle = document.querySelector(".password_field");
+const copyBtn = document.querySelector(".copy_btn");
+const generateBtn = document.querySelector(".generate_btn");
+
+// Creating Functions
+
+function generatePassowrd() {
+    console.log("Jello");
+    
+}
+
+function copyPassword() {
+    // Select the text field
+    passwordEle.select();
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(passwordEle.value);
+
+    // Aler the text
+    alert("Copied " + passwordEle.value);
+}
+
+// Calling Functions
+generateBtn.addEventListener("click", generatePassowrd);
+copyBtn.addEventListener("click", copyPassword);
