99573ff2ab0fdb5f67079c1db7691119fc93ca75
Owais-Sonija <owaissonija007@gmail.com>
2024-10-04 20:39:02 +0500

    App completed

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
+//Getting library
+
+
 // Getting Elements
 const passwordEle = document.querySelector(".password_field");
 const copyBtn = document.querySelector(".copy_btn");
@@ -6,8 +9,24 @@ const generateBtn = document.querySelector(".generate_btn");
 // Creating Functions
 
 function generatePassowrd() {
-    console.log("Jello");
+    let password= "";
+    const passwordLength = 12; 
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_";
+    const allChar = upperCaseLetters + lowerCaseLetters + numbers + symbols;
+
+    password += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]
+    password += numbers[Math.floor(Math.random() * numbers.length)]
+    password += symbols[Math.floor(Math.random() * symbols.length)]
+    while(password.length <= passwordLength) {
+        password += allChar[Math.floor(Math.random() * allChar.length)]
+    }
+passwordEle.value = password;    
     
+
 }
 
 function copyPassword() {
